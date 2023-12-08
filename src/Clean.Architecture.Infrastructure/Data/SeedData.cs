71321109ae410cc96bcd8734038097aecbf06022
Kyle McMaster <KyleMcMaster@users.noreply.github.com>
2023-12-08 12:39:02 -0500

    Add example for generating 100 Contributors

--- src/Clean.Architecture.Infrastructure/Data/SeedData.cs
+++ src/Clean.Architecture.Infrastructure/Data/SeedData.cs
@@ -1,4 +1,5 @@
-﻿using Clean.Architecture.Core.ContributorAggregate;
+﻿using Bogus;
+using Clean.Architecture.Core.ContributorAggregate;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -6,35 +7,26 @@ namespace Clean.Architecture.Infrastructure.Data;
 
 public static class SeedData
 {
-  public static readonly Contributor Contributor1 = new(
-      email: "Email1@Microsoft.com",
-      firstName: "Ardalis",
-      lastName: "Smith",
-      followers: 1,
-      following: 2,
-      stars: 3,
-      status: ContributorStatus.NotSet.Name);
-  public static readonly Contributor Contributor2 = new(
-      email: "Email1@Microsoft.com",
-      firstName: "Snowfrog",
-      lastName: "Vaillancourt",
-      followers: 4,
-      following: 5,
-      stars: 6,
-      status: ContributorStatus.NotSet.Name);
+  //public static readonly Contributor Contributor1 = new(
+  //    email: "Email1@Microsoft.com",
+  //    firstName: "FirstName1",
+  //    lastName: "LastName1",
+  //    followers: 1,
+  //    following: 2,
+  //    stars: 3,
+  //    status: ContributorStatus.NotSet.Name);
+  //public static readonly Contributor Contributor2 = new(
+  //    email: "Email2@Microsoft.com",
+  //    firstName: "FirstName2",
+  //    lastName: "LastName2",
+  //    followers: 4,
+  //    following: 5,
+  //    stars: 6,
+  //    status: ContributorStatus.NotSet.Name);
 
-  public static void Initialize(IServiceProvider serviceProvider)
-  {
-    using var dbContext = new AppDbContext(
-        serviceProvider.GetRequiredService<DbContextOptions<AppDbContext>>(), null);
-    // Look for any Contributors.
-    if (dbContext.Contributors.Any())
-    {
-      return; // DB has been seeded
-    }
-
-    PopulateTestData(dbContext);
-  }
+  private static readonly ContributorFaker _contributorFaker = new();
+  public static Contributor Contributor1 = _contributorFaker.Generate();
+  public static Contributor Contributor2 = _contributorFaker.Generate();
 
   public static void PopulateTestData(AppDbContext dbContext)
   {
@@ -48,5 +40,36 @@ public static class SeedData
     dbContext.Contributors.Add(Contributor2);
 
     dbContext.SaveChanges();
+  }
+
+  public static void Initialize(IServiceProvider serviceProvider)
+  {
+    using var dbContext = new AppDbContext(
+        serviceProvider.GetRequiredService<DbContextOptions<AppDbContext>>(), null);
+    // Look for any Contributors.
+    if (dbContext.Contributors.Any())
+    {
+      return; // DB has been seeded
+    }
+
+    PopulateTestData(dbContext);
+  }
+}
+
+public class ContributorFaker : Faker<Contributor>
+{
+  private int _initialId = 1;
+
+  public ContributorFaker()
+  {
+    CustomInstantiator(f => new Contributor(
+      email: f.Person.Email,
+      firstName: f.Person.FirstName,
+      lastName: f.Person.LastName,
+      followers: f.Random.Int(0, 100),
+      following: f.Random.Int(0, 100),
+      stars: f.Random.Int(0, 100),
+      status: f.PickRandom<ContributorStatus>(ContributorStatus.List).Name))
+      .RuleFor(o => o.Id, f => _initialId++);
   }
 }
--- tests/Clean.Architecture.FunctionalTests/ApiEndpoints/ContributorList.cs
+++ tests/Clean.Architecture.FunctionalTests/ApiEndpoints/ContributorList.cs
@@ -1,17 +1,54 @@
 ﻿using Ardalis.HttpClientTestExtensions;
+using Bogus;
 using Clean.Architecture.Infrastructure.Data;
 using Clean.Architecture.Web.Endpoints.ContributorEndpoints;
+using FluentAssertions;
 using Xunit;
 
 namespace Clean.Architecture.FunctionalTests.ApiEndpoints;
 
 [Collection("Sequential")]
-public class ContributorList(CustomWebApplicationFactory<Program> factory) : IClassFixture<CustomWebApplicationFactory<Program>>
+public class ContributorList(CustomWebApplicationFactory<Program> factory) 
+  : IClassFixture<CustomWebApplicationFactory<Program>>
 {
   private readonly HttpClient _client = factory.CreateClient();
 
   [Fact]
-  public async Task ReturnsTwoContributors()
+  public void GeneratesDefaultData()
+  {
+    var faker = new Faker<BogusEntity>();
+    var entity = faker.Generate();
+    //string json = System.Text.Json.JsonSerializer.Serialize(entity);
+  }
+
+  [Fact]
+  public void GeneratesConfiguredData()
+  {
+    var faker = new Faker<BogusEntity>()
+      .RuleFor(o => o.Id, f => f.Random.Guid())
+      .RuleFor(o => o.Name, f => f.Name.FullName())
+      .RuleFor(o => o.DateOfBirth, f => f.Date.Past());
+
+    var entity = faker.Generate();
+
+    // Generates something like this:
+    //{
+    //  "Id": "f5ec1d0f-f1de-398b-1c2b-592a4921cc04",
+    //  "DateOfBirth": "2023-10-31T22:04:22.7426011-04:00",
+    //  "Name": "Lora Kuvalis"
+    //}
+
+    var strictFaker = new Faker<BogusEntity>()
+      .StrictMode(true)
+      .RuleFor(o => o.Id, f => f.Random.Guid())
+      .RuleFor(o => o.Name, f => f.Random.String());
+
+    // Throws a Bogus.ValidationException since DateOfBirth is not configured
+    var strictEntity = strictFaker.Generate(); 
+  }
+
+  [Fact]
+  public async Task ReturnsTwoContributorsOriginal()
   {
     var result = await _client.GetAndDeserializeAsync<ContributorListResponse>("/Contributors");
 
@@ -19,4 +56,23 @@ public class ContributorList(CustomWebApplicationFactory<Program> factory) : ICl
     Assert.Contains(result.Contributors, i => i.Id == SeedData.Contributor1.Id);
     Assert.Contains(result.Contributors, i => i.Id == SeedData.Contributor2.Id);
   }
+
+  [Fact]
+  public void DemosGenerating100Contributors()
+  {
+    int count = 100;
+    var faker = new ContributorFaker();
+    var contributors = faker.Generate(count);
+
+    //string json = System.Text.Json.JsonSerializer.Serialize(contributors);
+
+    contributors.Should().HaveCount(count);
+  }
+}
+
+public class BogusEntity
+{
+  public Guid Id { get; set; }
+  public DateTime DateOfBirth { get; set; }
+  public string Name { get; set; } = "";
 }
